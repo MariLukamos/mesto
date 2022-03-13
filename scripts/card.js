07a3393efaf982c18bf9ef06f0bdcc1dc0e1eec0
Mari Luka <mlouka@bk.ru>
2022-03-13 10:37:21 +0300

    Fifthproject

---
+++ scripts/card.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+      name: "Озеро Тургояк",
+      link: "https://images.unsplash.com/photo-1608411404585-6bbd9c6cec96?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80",
+    },
+    {
+      name: "Озеро Зюраткуль",
+      link: "https://images.unsplash.com/photo-1637073247480-99b12c71d56a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    },
+    {
+      name: "Таганай",
+      link: "https://images.unsplash.com/photo-1610738918169-3df48c0a231c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80",
+    },
+    {
+      name: "Сад...",
+      link: "https://images.unsplash.com/photo-1510942752400-ebce99a8a2c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80",
+    },
+    {
+      name: "Сосновый лес",
+      link: "https://images.unsplash.com/photo-1635417006446-a99b4e952c2a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80",
+    },
+    {
+      name: "Миасс",
+      link: "https://images.unsplash.com/photo-1514400492509-eb84a50d38d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80",
+    },
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,65 @@
-const editProfilePopup = document.querySelector(".popup-profile");
-const profileTitle = document.querySelector(".profile__name");
-const profileAbout = document.querySelector(".profile__about");
-const profileForm = document.querySelector(".popup__user-form-edit");
-const nameInput = document.querySelector(".popup__input_type_username");
-const aboutInput = document.querySelector(".popup__input_type_about");
-const profileEditButton = document.querySelector(".profile__edit-button");
-const profileCloseButton = editProfilePopup.querySelector(".popup__close-button");
+const editProfilePopup = document.querySelector('.popup-profile');
+const profileTitle = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
+const profileForm = document.querySelector('.popup__user-form-edit');
+const nameInput = document.querySelector('.popup__input_type_username');
+const aboutInput = document.querySelector('.popup__input_type_about');
+const profileEditButton = document.querySelector('.profile__edit-button');
+const profileCloseButton = editProfilePopup.querySelector('.popup__close-button');
+const addCardPopup = document.querySelector('.popup-place');
+const showAddCardPopup = document.querySelector('.profile__add-button');
+const cardCloseButton = addCardPopup.querySelector('.popup__close-button');
+const formCard = document.querySelector('.popup__photo-add-form');
+const placeInput = document.querySelector('.popup__input_place_name');
+const linkInput = document.querySelector('.popup__input_place_url');
+const imagePopup = document.querySelector('.popup-photo');
+const popupImagePicture = imagePopup.querySelector('.popup__image');
+const popupImageTitle = imagePopup.querySelector('.popup__caption');
+const imageCloseButton = imagePopup.querySelector('.popup__close-button');
+const imageText = document.querySelector('.photo__caption');
+const cardTemplate = document.querySelector('.photo-grid-template');
+const cardContainer = document.querySelector('.photo-grid__list');
+
+function initialCardsRender(initialCards) {
+    cardContainer.prepend(createCard(initialCards.link, initialCards.name));
+}
+initialCards.map(initialCardsRender);
+  
+function addCard(evt) {
+    evt.preventDefault();
+    cardContainer.prepend(createCard(link.value, title.value));
+    closePopup(popupAddCard);
+}
+
+function createCard(url, desc) {
+    const newCard = cardTemplate.content.cloneNode(true);
+      newCard.querySelector('.photo-grid__image').src = url;
+      newCard.querySelector('.photo-grid__name').textContent = desc;
+      newCard.querySelector('.photo-grid__image').alt = desc;
+      newCard.querySelector('.photo-grid__remove-button').addEventListener('click', handlerCardDelete);
+      newCard.querySelector('.photo-grid__button-like').addEventListener('click', function(evt) {
+        evt.target.classList.toggle('photo-grid__button-like_activated');
+        });
+      newCard.querySelector('.photo-grid__image').addEventListener('click', function() {
+        openPopup(imagePopup);
+        popupImagePicture.src = url;
+        popupImageTitle.textContent = desc;
+        popupImagePicture.alt = desc;
+    })
+    return newCard;
+}
+
+function handlerCardDelete(evt) {
+    const elementDelete = evt.target.closest('.photo-grid__item');
+    elementDelete.remove();
+}
 
 function openPopup(popup) {
-    popup.classList.add("popup_opened");
+    popup.classList.add('popup_opened');
 }
   
 function closePopup(popup) {
-    popup.classList.remove("popup_opened");
+    popup.classList.remove('popup_opened');
 }
 
 function editProfile(event) {
@@ -26,12 +73,13 @@ function saveRedactorProfile(event) {
     event.preventDefault();
     profileTitle.textContent = nameInput.value;
     profileAbout.textContent = aboutInput.value;
-    nameInput.value = "";
-    aboutInput.value = "";
+    nameInput.value = '';
+    aboutInput.value = '';
     closePopup(editProfilePopup);
-  
 }
-
-profileEditButton.addEventListener("click", editProfile);
-profileCloseButton.addEventListener("click", () => closePopup(editProfilePopup));
-profileForm.addEventListener("submit", saveRedactorProfile);
+imageCloseButton.addEventListener('click', ()=>{closePopup(imagePopup)});
+cardCloseButton.addEventListener('click', ()=>{closePopup(addCardPopup)});
+showAddCardPopup.addEventListener('click', ()=>{openPopup(addCardPopup)});
+profileEditButton.addEventListener('click', editProfile);
+profileCloseButton.addEventListener('click', () => closePopup(editProfilePopup));
+profileForm.addEventListener('submit', saveRedactorProfile);
