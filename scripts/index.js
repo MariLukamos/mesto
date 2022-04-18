287dc7b28bd3f253d601934c317ea0679eba20f6
Mari Luka <mlouka@bk.ru>
2022-04-19 00:17:58 +0300

    SeventhProject

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-import { Card } from './Card.js';
+import Card from './Card.js';
 import { FormValidator, selectors } from './FormValidator.js';
 
 const editProfilePopup = document.querySelector('.popup-profile');
